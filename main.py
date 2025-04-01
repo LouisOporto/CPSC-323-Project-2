55737b1e6bc619d4279817f768e8bd18a141fdaa
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-03-31 23:58:17 -0700

    CFG rules and Parsing Table commented

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-# 
+# Create a parser given the alphabet
+# {i, +, -, *, /, ), (} and ending with $
+# Given a CFG and parsing table
+# CFG
+# E -> E + T
+# E -> E - T
+# E -> T
+# T -> T * F
+# T -> T / F
+# T -> F
+# F -> (E)
+# F -> a
+
+# First and Follow Table
+# ELEMENT | FIRST | FOLLOW
+#   E     |(, a   | $, )
+#   Q     |+, -, ɛ| $, )
+#   T     |(, a   | +, -, ), $
+#   R     |/, *, ɛ| +, -, ), $
+#   F     |(, a   | $
 
 if __name__ == "__main__":
-    print("Running LR parser\n");
+    print("Running LR parser\n")
