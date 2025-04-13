8e8bf4c21ad1b47f98b90c2a16960b90ac4ca438
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-13 13:30:24 -0700

    Change recursion table

--- main.py
+++ main.py
@@ -13,30 +13,73 @@
 # F -> a
 
 # Left-recursion rules elimination
-# E -> TQ
-# Q -> +TQ
-# Q -> -TQ
-# Q -> ɛ
-# T -> FR
-# R -> *FR
-# R -> /FR
-# R -> ɛ
+# E -> TE'
+# E' -> T + E'
+# E' -> T - E'
+# E' -> ɛ
+
+# T -> FT'
+# T' -> F * T'
+# T' -> F / T'
+# T' -> ɛ
+
 # F -> (E)
 # F -> a
 
 # First and Follow Table
-# ELEMENT | FIRST | FOLLOW
-#   E     |(, a   | $, )
-#   Q     |+, -, ɛ| $, )
-#   T     |(, a   | +, -, ), $
-#   R     |/, *, ɛ| +, -, ), $
-#   F     |(, a   | $
+# ELEMENT  |  FIRST  | FOLLOW
+#    E     | (, a    | $, )
+#    E'    | +, -, ɛ | $, ) 
+#    T     | (, a    | +, -, ), $
+#    T'    | /, *, ɛ | +, -, ), $
+#    F     | (, a    | $
 
 # Input Examples: (remove whitespace)
 # "(a+a)*a$"
 # "a*(a/a)$"
 # "a(a+a)$"
 
+# Invalid method need to change this but an idea
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token_index = 0 
+    
+    def _get_next_token(self):
+        if self.current_token_index < len(self.tokens)
+            return self.tokens[self.current_token_index]
+        return None
+    
+    def _consume_token(self):
+        self.current_token_index += 1
+
+    def parse(self):
+        return self.parse_expression()
+    
+    def parse_expression(self):
+        left_term = self.parse_term()
+        while self._get_next_token in "+-":
+            op = self._get_next_token()
+            self._consume_token()
+            right_term = self.prase_term()
+            left_term = self.self.evaluate(left_term, op, right_term)
+        return left_term
+    
+    def parse_term(self):
+        token = self._get_next_token()
+        self._consume_token()
+        return int(token)
+    
+    def evaluate(self, left, op, right):
+        if op == "+":
+            return left + right
+        if op == "-":
+            return left - right
+        if op == "*":
+            return left * right
+        if op == "/":
+            return left * right
+        
 def tokenize(input_string: str) -> list:
     tokens = []
     current_number = ""
